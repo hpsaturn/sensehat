81de737b781cd1e2a6834d808b243542073e7d1f
Antonio Vanegas <hpsaturn@gmail.com>
2018-04-29 13:25:08 -0500

    add basic runneable for random color

--- app/src/main/java/hpsaturn/sensehat/matrixled/MainActivity.java
+++ app/src/main/java/hpsaturn/sensehat/matrixled/MainActivity.java
@@ -3,14 +3,19 @@ package hpsaturn.sensehat.matrixled;
 import android.app.Activity;
 import android.graphics.Color;
 import android.os.Bundle;
+import android.os.Handler;
+import android.util.Log;
 
 import com.google.android.things.contrib.driver.sensehat.LedMatrix;
 import com.google.android.things.contrib.driver.sensehat.SenseHat;
 
 import java.io.IOException;
+import java.util.Random;
 
 public class MainActivity extends Activity {
     private static final String TAG = MainActivity.class.getSimpleName();
+    private LedMatrix display;
+    private Handler mHandler;
 
 
     @Override
@@ -19,17 +24,48 @@ public class MainActivity extends Activity {
 
         // Color the LED matrix.
         try {
-            LedMatrix display = SenseHat.openDisplay();
-            display.draw(Color.MAGENTA);
-            display.close();
+            display = SenseHat.openDisplay();
+            display.draw(Color.RED);
         } catch (IOException e) {
             e.printStackTrace();
         }
 
+        mHandler = new Handler();
+        mHandler.post(mRandomColorRunnable);
+
     }
+
+    private Runnable mRandomColorRunnable = new Runnable() {
+
+        private static final long DELAY_MS = 100L;
+
+        @Override
+        public void run() {
+            if (display == null) {
+                return;
+            }
+            try {
+                Random rnd = new Random();
+                int color = Color.argb(128, rnd.nextInt(256), rnd.nextInt(256), rnd.nextInt(256));
+                display.draw(color);
+                mHandler.postDelayed(this, DELAY_MS);
+
+            } catch (IOException e) {
+                Log.e(TAG,"error setting color");
+                e.printStackTrace();
+            }
+        }
+    };
+
 
     @Override
     protected void onDestroy() {
+        try {
+            display.close();
+        } catch (IOException e) {
+            Log.e(TAG,"error on display close");
+            e.printStackTrace();
+        }
         super.onDestroy();
     }
 
